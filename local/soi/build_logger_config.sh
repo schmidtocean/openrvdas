18549aef7edfb7c64cce53365982d415cdc73c0d
Webb Pinner <webbpinner@gmail.com>
2024-02-09 07:19:56 -0400

    initial import

---
+++ local/soi/build_logger_config.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+OPENVDM_SERVER_URL="http://10.23.9.20/"
+
+OPENRVDAS_CONFIG_TEMPLATE="/opt/openrvdas/local/soi/logger_config.template"
+OPENRVDAS_CONFIG_DEST="/opt/openrvdas/local/soi"
+OPENRVDAS_CONFIG_BACKUP_DEST=/home/mt/openrvdas_config_backups
+OPENRVDAS_CONFIG_FN="logger_config"
+HDR_DIR="/opt/openrvdas/local/soi/header-files"
+
+PWD=`pwd`
+
+precheck() {
+
+  if [[ ! -f "${OPENRVDAS_CONFIG_TEMPLATE}" ]]; then
+    echo "ERROR: could not find template file... exiting"
+    return
+  fi
+
+  if [[ ! -d "${OPENRVDAS_CONFIG_DEST}" ]]; then
+    echo "ERROR: could not find destination directory... exiting"
+    return
+  fi
+
+  if [[ ! -d "${OPENRVDAS_CONFIG_DEST}/backups" ]]; then
+    echo "ERROR: could not find backup directory... exiting"
+    return
+  fi
+
+}
+
+query_api() {
+
+  CRUISE_ID=`curl -s "${OPENVDM_SERVER_URL}api/warehouse/getCruiseID" |
+    python3 -c "import sys, json; print(json.load(sys.stdin)['cruiseID'])"`
+
+  echo "Cruise ID: ${CRUISE_ID}"
+
+  CRUISE_START_DATE=`curl -s "${OPENVDM_SERVER_URL}api/warehouse/getCruiseStartDate" |
+    python3 -c "import sys, json; print(json.load(sys.stdin)['cruiseStartDate'].split()[0])" | sed 's?/?-?g'`
+
+  echo "Cruise Start Date: ${CRUISE_START_DATE}"
+
+  CRUISE_END_DATE=`curl -s "${OPENVDM_SERVER_URL}api/warehouse/getCruiseEndDate" |
+    python3 -c "import sys, json; print(json.load(sys.stdin)['cruiseEndDate'].split()[0])" | sed 's?/?-?g'`
+
+  echo "Cruise End Date: ${CRUISE_END_DATE}"
+
+}
+
+build_config_file() {
+
+  sed -e "s/{cruiseID}/$CRUISE_ID/g" \
+      -e "s|{cruiseStartDate}|$CRUISE_START_DATE|g" \
+      -e "s|{cruiseEndDate}|$CRUISE_END_DATE|g" \
+      -e "s|{headerDir}|$HDR_DIR|g" \
+      ${OPENRVDAS_CONFIG_TEMPLATE} > ${OPENRVDAS_CONFIG_DEST}/${OPENRVDAS_CONFIG_FN}.yaml
+
+  cd ${OPENRVDAS_CONFIG_DEST}
+  tar -czf ${CRUISE_ID}_openrvdas_backup.tar.gz ${OPENRVDAS_CONFIG_FN}.yaml devices ship_devices.yaml sb_devices.yaml
+  if [[ ! -f "${CRUISE_ID}_openrvdas_backup.tar.gz" ]]; then
+    echo "ERROR: could not create cruise-specific backup"
+    return
+  fi
+
+  mv ${CRUISE_ID}_openrvdas_backup.tar.gz ${OPENRVDAS_CONFIG_BACKUP_DEST}
+
+}
+
+main() {
+
+  precheck
+  query_api
+  build_config_file
+
+  cd $PWD  
+}
+
+# -----------------------------------------------------------------------------
+# Run
+# -----------------------------------------------------------------------------
+
+main "$@"
+
